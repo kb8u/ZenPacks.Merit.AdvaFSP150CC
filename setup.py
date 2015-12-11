4ddda986c2bf6d4bd3c45410d2f268cafeed7da7
Russell Dwarshuis <rjd@merit.edu>
2015-12-11 06:21:01 -0500

    added new models

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.Merit.AdvaFSP150CC"
-VERSION = "1.1.0"
+VERSION = "1.2.0"
 AUTHOR = "Russell Dwarshuis"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.Merit']
